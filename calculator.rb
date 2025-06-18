a9cf1bcfea38e9ef225c88c1d2a8802373174fd9
Lopesh <durugkarlopesh@gmail.com>
2025-06-18 16:25:50 +0530

    - Code optimization.

--- calculator.rb
+++ calculator.rb
@@ -5,13 +5,12 @@ class Calculator
       return 0 if input.empty?
       
       ## This will handle for delimiters like // etc.
-      if input.match(/^\/\/(.+)\n/)
-        delimiter_line, input = input.split("\n", 2)
-        delimiter = Regexp.escape(delimiter_line[2])
-        delimiters = /#{delimiter}|,|\n/
-      else
-        delimiters = /;|,|\n/
-      end
+      delimiters =  if input.match(/^\/\/(.+)\n/)
+                      delimiter_line, input = input.split("\n", 2)
+                      /#{Regexp.escape(delimiter_line[2])}|,|\n/
+                    else
+                      /;|,|\n/
+                    end
 
       numbers = input.split(delimiters).map(&:to_i)
       numbers.sum
