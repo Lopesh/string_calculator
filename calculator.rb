e3d6632cf22ee238504245004999845af34338e9
Lopesh <durugkarlopesh@gmail.com>
2025-06-18 16:23:36 +0530

    - Handle // delimiters for addition.

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,18 @@
+require 'pry'
+
 class Calculator
     def self.add(input)
       return 0 if input.empty?
-      delimiters = /,|\n/
-  
+      
+      ## This will handle for delimiters like // etc.
+      if input.match(/^\/\/(.+)\n/)
+        delimiter_line, input = input.split("\n", 2)
+        delimiter = Regexp.escape(delimiter_line[2])
+        delimiters = /#{delimiter}|,|\n/
+      else
+        delimiters = /;|,|\n/
+      end
+
       numbers = input.split(delimiters).map(&:to_i)
       numbers.sum
     end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -21,5 +21,9 @@ RSpec.describe Calculator do
     it "supports newlines between numbers" do
       expect(described_class.add("1\n2,3")).to eq(6)
     end
+
+    it "supports custom delimiter like //;\n1;2" do
+      expect(described_class.add("//;\n1;2")).to eq(3)
+    end
   end
 end
