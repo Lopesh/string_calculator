14816c26898354c6e22a79fee403c3c879b496d6
Lopesh <durugkarlopesh@gmail.com>
2025-06-18 16:01:25 +0530

    - Added test case for by passing single number.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Calculator do
     it "returns 0 for an empty string" do
       expect(described_class.add("")).to eq(0)
     end
+
+    it "returns the number itself for a single number" do
+      expect(described_class.add("5")).to eq(5)
+    end
   end
 end
